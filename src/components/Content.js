d34baa6c21ecf43eec3dd42e0c60dc3a292d5419
souravchiku <76004164+souravchiku@users.noreply.github.com>
2021-05-08 10:44:23 +0530

    minor

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,18 @@
-import react from 'react'
+import react from "react";
 
-const Content=()=>{
+const Content = () => {
   return (
     <div className="content">
-      <h3 style={{borderBottom:"1px solid grey", padding:"10px"}}>  Hey !! Welcome to Application...</h3>
-      <h4> Click on<b> Gallery</b> for Photos and if you are feeling low then click on <b>jokes</b> </h4>
-      </div>
-  )
-}
-export default Content
+      <h3 style={{ borderBottom: "1px solid grey", padding: "10px" }}>
+        {" "}
+        Hey !! Welcome to Application...
+      </h3>
+      <h4>
+        {" "}
+        Click on<b> Gallery</b> for Searching Photo's and if you are feeling low
+        then click on <b>jokes</b>{" "}
+      </h4>
+    </div>
+  );
+};
+export default Content;
